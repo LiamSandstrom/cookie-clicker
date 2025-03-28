1b18e389e1335c2cffb913c813659affb0447d81
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-03-29 00:20:39 +0100

    add everything inside DOMContentLoaded

--- javascript.js
+++ javascript.js
@@ -1,24 +1,34 @@
-let btnRef = document.querySelector("button");
-let h1ref = document.getElementById("score");
+document.addEventListener("DOMContentLoaded", function() {
+    let btnRef = document.getElementById("cookie");
+    let h1ref = document.getElementById("score");
+    let ref2x = document.getElementById("2x");
+    let ref1s = document.getElementById("1s");
+    let ref3rd = document.getElementById("3rd");
 
-let multi = 1;
+    let multi = 1;
 
-let scoreObj = {
-    _score : 0,
-    get score() {
-        return this._score;
-    },
-    set score(value) {
-        this._score += value;
-        updateScore();
+    let scoreObj = {
+        _score : 0,
+        get score() {
+            return this._score;
+        },
+        set score(value) {
+            this._score += value;
+            updateScore();
+        }
     }
-}
 
-btnRef.addEventListener("click" , () => {
-    scoreObj.score = 1 * multi;
-}
-);
+    ref2x.addEventListener("click", () => {
+        multi += 2;
+    });
 
-function updateScore(){
-    h1ref.textContent = scoreObj.score;
-}
+    btnRef.addEventListener("click" , () => {
+        scoreObj.score = 1 * multi;
+    }
+    );
+
+    function updateScore(){
+        h1ref.textContent = scoreObj.score;
+    }
+});
+
